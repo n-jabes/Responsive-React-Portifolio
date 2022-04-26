748fdc5c6ec0b6159eff13f7f3d335854cdd64a9
n-jabes <nshutij7@gmail.com>
2022-04-26 07:43:16 +0200

    finished the footer component and it is the end of the project now it is ready to be deployed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './Footer.css'
+import {FaFacebook} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {IoLogoTwitter} from 'react-icons/io'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>LOGO</a>
+
+      <ul className='permalinks'>
+        <li href='#'>Home</li>
+        <li href='#about'>About</li>
+        <li href='#experience'>Experience</li>
+        <li href='#services'>Services</li>
+        <li href='#portfolio'>Portfolio</li>
+        <li href='#testimonials'>Testimonials</li>
+        <li href='#contact'>Contact</li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com"><FaFacebook /></a>
+        <a href="https://instagram.com"><FiInstagram /></a>
+        <a href="https://twitter.com"><IoLogoTwitter /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; EGATOR Tutorials. All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
